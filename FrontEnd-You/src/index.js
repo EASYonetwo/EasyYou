bd5b20ee090d1fbd8c3e6e66ea213e54fe3d2b0e
YTK <a@a.net>
2024-05-01 23:17:18 +0900

    24.05.01/Member+Login

--- FrontEnd-You/src/index.js
+++ FrontEnd-You/src/index.js
@@ -6,7 +6,10 @@ import './css/CompHeader.css'
 import './css/CompMain.css'
 import './css/CompFooter.css'
 import './css/CompBoardD.css'
+import './css/CompLogin.css'
 import './css/style.css'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
