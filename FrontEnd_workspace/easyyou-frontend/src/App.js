a684097b362603b79b0f78e027626e2e7c017b11
YTK <a@a.net>
2024-05-06 20:45:08 +0900

    5월6일/불러오기

--- FrontEnd_workspace/easyyou-frontend/src/App.js
+++ FrontEnd_workspace/easyyou-frontend/src/App.js
@@ -8,23 +8,47 @@ import CompYou from './component/Portfolio/CompYou';
 import CompEasy from './component/Portfolio/CompEasy';
 import CompLogin from './component/CompLogin';
 import CompMember from './component/CompMember';
+import { createContext, useEffect, useState } from 'react';
+export const AppContext = createContext()
 
-
+function UserId() {
+  let userId
+  let userLogin
+  if (window.localStorage.getItem('UserStorage')) {
+    userId = JSON.parse(window.localStorage.getItem('UserStorage'))
+    userLogin = true
+  }
+  else {
+    userId = ''
+    userLogin = false
+  }
+  return { userId, userLogin }
+}
 
 function App() {
+  const [_isLogin, _setIsLogin] = useState(UserId().userLogin)
+  const [_loginId, _setLoginId] = useState(UserId().userId.id)
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.localStorage.setItem('UserStorage','')
+    },1000*30)
+  })
   return (
     <div className="App">
       <BrowserRouter>
-        <CompHeader></CompHeader>
-        <Routes>
-          <Route path='/' element={<CompMain />} />
-          <Route path='/BoardD' element={<CompBoardD />} />
-          <Route path='/BoardF' element={<CompBoardF />} />
-          <Route path='/YouPort' element={<CompYou />} />
-          <Route path='/EasyPort' element={<CompEasy />} />
-          <Route path='/login' element={<CompLogin />} />
-          <Route path='/Member' element={<CompMember />} />
-        </Routes>
+        <AppContext.Provider value={{ _isLogin, _setIsLogin, _loginId, _setLoginId }}>
+          <CompHeader></CompHeader>
+          <Routes>
+            <Route path='/' element={<CompMain />} />
+            <Route path='/BoardD' element={<CompBoardD />} />
+            <Route path='/BoardF' element={<CompBoardF />} />
+            <Route path='/YouPort' element={<CompYou />} />
+            <Route path='/EasyPort' element={<CompEasy />} />
+            <Route path='/login' element={<CompLogin />} />
+            <Route path='/Member' element={<CompMember />} />
+          </Routes>
+        </AppContext.Provider>
         <CompFooter></CompFooter>
       </BrowserRouter>
     </div>
--- FrontEnd_workspace/easyyou-frontend/src/component/DFpost/CompFHpost.jsx
+++ FrontEnd_workspace/easyyou-frontend/src/component/DFpost/CompFHpost.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const CompFHpost = () => {
+const CompFHpost = ({data}) => {
+  const {file,title,user,regdate} = data
   return (
     <div className='Fpost'>
       <span className='Hot'> Hot</span>
-      <p className='Mimg'><img src="" alt="" />이미지</p>
-      <p className='Text'>제목</p>
-      <p className='Person'>작성자</p>
-      <p className='Day'>날짜</p>
+      <p className='Mimg'><img src='' alt="" onClick={()=>{window.open({file})}}/></p>
+      <p className='Text'>{title}</p>
+      <p className='Person'>{user.id}</p>
+      <p className='Day'>{regdate.split('T15',1)}</p>
     </div>
   );
 };
