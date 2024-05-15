43802a1e7b49afb825d0522459380555a67328b7
YTK <a@a.net>
2024-05-15 11:55:31 +0900

    5.15일

--- FrontEnd_workspace/easyyou-frontend/src/component/BoardF/CompBoardFDF.jsx
+++ FrontEnd_workspace/easyyou-frontend/src/component/BoardF/CompBoardFDF.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CompBoardFDF = ({ data}) => {
-  const { filename, filesize} = data
+const CompBoardFDF = ({ data }) => {
+  const { filename, filesize, file } = data
+  const [_form, _setForm] = useState()
+  const [_isDownloading,_setIsDownloading] = useState()
 
+  useEffect(() => {
+    _setForm(data.filename.split('.')[data.filename.split('.').length - 1])
+  }, [_form, data])
 
-
-
-
-
-
-
+  const fnDownload = () => {
+  };
 
 
   return (
     <div>
       <div className='main-img'  >
-        <img src="" alt="" />
+        {(_form) && (_form === 'jpg' || _form === 'png' || _form === 'jpeg') ? <img src={`data:image/${_form};base64,${file}`} alt="" /> : <img src="/img/notImg.png" alt="" />}
       </div>
       <div className='main-file'>
         <span className='name'>{(data) && filename}</span>
-        <span className='size'>{(data) && filesize}</span>
+        <span className='size'>[{(data) && filesize}] byte </span>
       </div>
-      <button >다운로드</button>
+      <button onClick={fnDownload} >다운로드</button>
     </div>
   );
 };
