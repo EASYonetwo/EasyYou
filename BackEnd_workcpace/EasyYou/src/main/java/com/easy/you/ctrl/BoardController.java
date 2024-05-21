ea6e0f5385601315cc06384ff97f5be88954c4a5
easy <2jiwons2@gmail.com>
2024-05-21 17:10:01 +0900

    240521 17:09 이지원 main file 전송시 filename 추가

--- BackEnd_workcpace/EasyYou/src/main/java/com/easy/you/ctrl/BoardController.java
+++ BackEnd_workcpace/EasyYou/src/main/java/com/easy/you/ctrl/BoardController.java
@@ -1,7 +1,6 @@
 package com.easy.you.ctrl;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
--- BackEnd_workcpace/EasyYou/src/main/java/com/easy/you/repository/BoardLikesRepository.java
+++ BackEnd_workcpace/EasyYou/src/main/java/com/easy/you/repository/BoardLikesRepository.java
@@ -17,11 +17,11 @@ public interface BoardLikesRepository extends JpaRepository<BoardLikesVo, Long>{
 			+ "ORDER BY COUNT(bl.board.boardseq) DESC, b.regdate DESC")
 	List<BoardRepositoryInterface> findTop3MostLikedByBtypeR(Pageable pageable);
 
-	@Query("SELECT b.boardseq AS boardseq, b.user AS user, b.title AS title, b.content AS content, b.btype AS btype, b.delflag AS delflag, b.regdate AS regdate , f.file AS file "
+	@Query("SELECT b.boardseq AS boardseq, b.user AS user, b.title AS title, b.content AS content, b.btype AS btype, b.delflag AS delflag, b.regdate AS regdate , f.file AS file ,  f.filename AS filename "
 			+ "FROM BoardVo b JOIN BoardLikesVo bl ON b.boardseq = bl.board.boardseq "
 			+ "LEFT JOIN FileStorageVo f ON f.board.boardseq = b.boardseq "
 			+ "WHERE b.btype = 'F' AND b.delflag = 'N' " 
-			+ "GROUP BY b.boardseq, f.file, f.board.boardseq "
+			+ "GROUP BY b.boardseq, f.file, f.board.boardseq, f.filename "
 			+ "ORDER BY COUNT(bl.board.boardseq) DESC, b.regdate DESC")
 	List<BoardRepositoryInterfaceFile> findTop3MostLikedByBtypeF();
 
